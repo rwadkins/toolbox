42063909275ffc8c702473c8ecbb6941bad93ee0
Ray Wadkins <rwadkins@twors.net>
2015-05-15 10:48:59 -0400

    fConsole.js
    fConsole script for writing console-like messages to the output panel in jsfiddle.net

---
+++ fConsole.js
@@ -0,0 +1,27 @@
+var fConsole = (function(){
+    function writeMessage(node, message) {
+        var elem = document.createElement("div");
+        elem.innerHTML = message;
+
+        node.appendChild(elem);
+    }
+
+    var _node = document.body;
+    var _logNode = null;
+
+    var fConsole = {
+        log : function() {
+            var output = Array.prototype.map.call(arguments, function(arg) {
+                return arg.toString();
+            });
+
+            writeMessage(_logNode || _node, output.join(" | "));
+        }
+    };
+
+    fConsole.log.node = function(node) {
+        _logNode = node;
+    };
+
+    return fConsole;
+})();
